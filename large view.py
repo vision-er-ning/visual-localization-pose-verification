db19c860e959ab15925726d27180d1cc53199e11
Vision-er <32409985+vision-er-ning@users.noreply.github.com>
2022-07-19 15:32:21 +0800

    Add files via upload

---
+++ large view.py
@@ -0,0 +1,64 @@
+from cProfile import label
+from scipy import signal
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import *
+from pandas import DataFrame,Series
+#file = open('C:/Users/Administrator/Desktop/语义定位手稿/gyr_synthetic_tran-2.txt')
+x_deepnavi=[0, 0.25, 0.5, 0.75,   1,   1.25, 1.5,  1.75,    2]
+
+
+y_inloc = [0, 0.324, 0.56, 0.652, 0.735, 0.788,  0.829, 0.838, 0.845]
+y_GSPV =  [0, 0.342, 0.573, 0.692, 0.777, 0.826,  0.855, 0.884, 0.901]
+
+y_HAIL =  [0, 0.315, 0.48, 0.61, 0.675, 0.7, 0.712,  0.719,  0.7235]
+
+y_efiloc= [0, 0.33, 0.5365, 0.635, 0.715, 0.763, 0.795, 0.815, 0.83]
+y_efilocpv=[0, 0.352,0.586, 0.715, 0.792,0.818, 0.843, 0.861, 0.8735]
+# for line in file:
+#     if line != "\n":
+#         data.append(line)
+# print(len(data))
+#data = file.readlines()
+#画图
+#figure(figsize=(16,8),dpi=200)
+
+rcParams['font.sans-serif']=['SimHei'] #显示中文字符
+rcParams['axes.unicode_minus'] = False
+
+config = {
+    "font.family":'Time New',  # 设置字体类型
+    #"font.size": ,
+#     "mathtext.fontset":'stix',
+}
+rcParams.update(config)
+
+plt.xlabel('Distance threshold (meters)',size = 18)#横坐标命名
+plt.ylabel('CDF',size = 20)
+# x=np.arange(0.2,3)
+plt.axis([0.2, 2, 0,1])
+
+
+
+#plt.plot(data0_1000,'b',label='Gyroscope data')
+#plt.plot(data1007_2025,'b',label='Gyroscope data')
+plt.plot(x_deepnavi,y_inloc,'b',label='InLoc',linestyle='-.',marker='o')
+plt.plot(x_deepnavi,y_GSPV,'y',label='GSPV',Marker='^')   #linestyle='--'
+plt.plot(x_deepnavi,y_HAIL,'purple',label='HAIL',linestyle='-',Marker='s')
+plt.plot(x_deepnavi,y_efiloc,'green',label='EfiLoc',Marker='v') #linestyle=':',
+plt.plot(x_deepnavi,y_efilocpv,'r',label='EfiLocPV',Marker='*')
+
+#plt.gcf().set_facecolor(np.ones(3)*240/255)   #生成画布网格大小
+plt.grid()  #生成网格
+plt.title("Large view", fontsize=16)  # 标题
+#plt.legend(loc='lower right',fontsize=14)#标签显示位置和大小   upper, lower
+plt.subplots_adjust(top=0.936, bottom=0.111, right=0.838, left=0.251, hspace=0, wspace=0)
+plt.savefig("Largeview-LOC.png", dpi = 500)
+plt.show()
+
+# markeredgecolor 或 mec 标记边缘颜色
+# markeredgewidth 或 mew 标记边缘宽度
+# markerfacecolor 或 mfc 标记面颜色
+# markerfacecoloralt 或 mfcalt
+# markersize 或 ms 标记大小
---
+++ viewpoint.py
@@ -0,0 +1,60 @@
+from cProfile import label
+from scipy import signal
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pylab import *
+from pandas import DataFrame,Series
+#file = open('C:/Users/Administrator/Desktop/语义定位手稿/gyr_synthetic_tran-2.txt')
+x_deepnavi=[0, 0.25, 0.5, 0.75,   1,   1.25, 1.5,  1.75,    2]
+y_inloc = [0, 0.35, 0.59, 0.68, 0.75, 0.82,  0.89, 0.92, 0.935]
+y_GSPV =  [0, 0.42, 0.65, 0.74, 0.82, 0.86,  0.91, 0.938, 0.945]
+y_HAIL =  [0, 0.29, 0.46, 0.58, 0.65, 0.71, 0.79,  0.83,  0.842]
+y_efiloc= [0, 0.33, 0.53, 0.67, 0.78, 0.83, 0.87, 0.90, 0.912]
+y_efilocpv=[0, 0.41,0.66, 0.75, 0.806,0.868, 0.923, 0.935, 0.938]
+# for line in file:
+#     if line != "\n":
+#         data.append(line)
+# print(len(data))
+#data = file.readlines()
+#画图
+#figure(figsize=(16,8),dpi=200)
+
+rcParams['font.sans-serif']=['SimHei'] #显示中文字符
+rcParams['axes.unicode_minus'] = False
+
+config = {
+    "font.family":'Time New',  # 设置字体类型
+    #"font.size": ,
+#     "mathtext.fontset":'stix',
+}
+rcParams.update(config)
+
+plt.xlabel('Distance threshold (meters)',size = 18)#横坐标命名
+plt.ylabel('CDF',size = 20)
+# x=np.arange(0.2,3)
+plt.axis([0.2, 2, 0,1])
+
+
+
+#plt.plot(data0_1000,'b',label='Gyroscope data')
+#plt.plot(data1007_2025,'b',label='Gyroscope data')
+plt.plot(x_deepnavi,y_inloc,'b',label='InLoc',linestyle='-.',marker='o')
+plt.plot(x_deepnavi,y_GSPV,'y',label='GSPV',Marker='^')   #linestyle='--'
+plt.plot(x_deepnavi,y_HAIL,'purple',label='HAIL',linestyle='-',Marker='s')
+plt.plot(x_deepnavi,y_efiloc,'green',label='EfiLoc',Marker='v') #linestyle=':',
+plt.plot(x_deepnavi,y_efilocpv,'r',label='EfiLocPV',Marker='*')
+
+#plt.gcf().set_facecolor(np.ones(3)*240/255)   #生成画布网格大小
+plt.grid()  #生成网格
+plt.title("Viewpoint", fontsize=16)  # 标题
+#plt.legend(loc='lower right',fontsize=14)#标签显示位置和大小   upper, lower
+plt.subplots_adjust(top=0.936, bottom=0.111, right=0.838, left=0.251, hspace=0, wspace=0)
+plt.savefig("viewpoint-LOC.png", dpi = 500)
+plt.show()
+
+# markeredgecolor 或 mec 标记边缘颜色
+# markeredgewidth 或 mew 标记边缘宽度
+# markerfacecolor 或 mfc 标记面颜色
+# markerfacecoloralt 或 mfcalt
+# markersize 或 ms 标记大小
